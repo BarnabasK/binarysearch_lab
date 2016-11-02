385e14da8f6ca35626ab9541877fa9989ade5b13
= <=>
2016-11-02 14:22:04 +0300

    Added comments to make the code more readable

--- binarysearch.py
+++ binarysearch.py
@@ -1,6 +1,12 @@
+'''
+BinarySearch, is a search algorithm that 
+finds the position of a target value within the list.
+'''
 class BinarySearch(list):
-
-  def __init__(self, a, b): #Creates list of length a with steps of b between elements
+  '''
+  Creates list of length a with steps of b between elements
+  '''
+  def __init__(self, a, b):
     self.a = a
     self.b = b
          
@@ -10,7 +16,10 @@ class BinarySearch(list):
       num +=1
   
     self.length = num
-
+    
+    '''
+    Implements the Binary Search Algorithm
+    '''
   def search(self,value):
     first = 0
     last = self.length-1
@@ -23,21 +32,25 @@ class BinarySearch(list):
     except ValueError:
       index = -1
       in_list = False
-
+      '''
+      1. Compares the middle element of the list with the target value
+      2. The search continues in the lower or upper half of the list
+      '''
     while first<=last and not found and in_list and value != self[last]:
         midpoint = (first+last)//2
         mid_value = self[midpoint]
         if mid_value == value:
             found = True
             count +=1
-            index = midpoint
+            index = midpoint    
         else:
-            if value < mid_value:
+          if value < mid_value:
                 last = midpoint - 1
                 count +=1
-            else:
+          else:
                 first = midpoint + 1
-                count +=1
-    
-    
-    return {"count": count, "index": index}    
+                count +=1    
+    '''
+    Returns the number of iterations and the index of the found value in the list
+    '''
+    return {"count": count, "index": index}       
